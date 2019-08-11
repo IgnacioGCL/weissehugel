3af55fccabe38520e1d9c1a9cc4a9444462b4aa4
IgnacioGCL <ignaciogclavazza@hotmail.es>
2019-08-11 21:41:04 +0200

    More changes

--- _site/index.js
+++
@@ -1,7 +0,0 @@
-function selectBackgroundPrev(woodSelector) {
-    const woodPrev = document.getElementById('wood-prev');
-    const woodText = document.getElementById('wood-text');
-    const woodName = woodSelector.children[0].innerHTML;
-    woodPrev.style.backgroundImage = woodSelector.style.backgroundImage;
-    woodText.innerHTML = woodName;
-}
